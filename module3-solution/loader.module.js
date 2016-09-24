9b0244955393d975ab6ae4ba8622b39b3a676dd6
Ivan <neradovicivan@yahoo.com>
2016-09-25 01:24:50 +0200

    Commiting Module 3 solution

---
+++ module3-solution/loader.module.js
@@ -0,0 +1,8 @@
+(function () {
+'use strict';
+
+	angular.module('LoaderApp', []);
+
+	//console.log("Initializing loader.module..");
+
+})();
---
+++ module3-solution/narrow.module.js
@@ -0,0 +1,9 @@
+(function () {
+'use strict';
+
+	angular.module('NarrowItDownApp', ['LoaderApp'])
+		.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+		
+	//console.log("Initializing narrow.module..");
+
+})();
